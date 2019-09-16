d559bf75fc6da9397ab6808ba6f26984b41c707f
JSG1987 <JGentry0058@gmail.com>
2019-09-15 22:32:49 -0500

    almostDone

--- MakeChange/Source.cpp
+++ MakeChange/Source.cpp
@@ -14,7 +14,7 @@ int main()
 
 	float cost;
 	float payment;
-	int y;
+	int dollars;
 	int n;
 
 ;
@@ -25,18 +25,70 @@ int main()
 	cin >> cost;
 	cout << "Enter amount you paid\n";
 	cin >> payment;
-	y = (payment - cost)*100;
-	n = (payment - cost) * 100;
+	dollars = (payment - cost)*100;
+	n = (payment - cost)*100;
 	
-	cout << "You will need " << y / 100 << " dollars\n";
-	y = y % 100;
-	cout << "You will need " << y / 25 << " Quarters\n";
-	y = y % 25;
-	cout << "you will need " << y / 10 << " dimes\n";
-	y = y % 10;
-	cout << "you will need " << y / 5 << " nickels\n";
-	y = y % 5;
-	cout << "you will need " << y / 1 << " pennies\n";
+	/*cout << "Your change is " << dollars / 100 << " dollars ";
+	dollars = dollars % 100;
+	cout <<  dollars / 25 << " Quarters ";
+	dollars = dollars % 25;
+	cout <<  dollars / 10 << " dimes ";
+	dollars = dollars % 10;
+	cout <<   dollars / 5 << " nickels ";
+	dollars = dollars % 5;
+	cout <<  dollars / 1 << " pennies ";
+	*/
+	if (dollars / 100 != 0)
+	{
+		dollars / 100;
+		dollars = dollars % 100;
+		cout <<"Your change is "<< dollars / 25 << " Quarters ";
+		dollars = dollars % 25;
+		cout << dollars / 10 << " dimes ";
+		dollars = dollars % 10;
+		cout << dollars / 5 << " nickels ";
+		dollars = dollars % 5;
+		cout << dollars / 1 << " pennies ";
+	}
+	else if (dollars / 25 != 0)
+	{
+		dollars / 100;
+		dollars = dollars % 100;
+		dollars / 25;
+		dollars % 25;
+		cout << "your change is "<< dollars / 10 << " dimes ";
+		dollars = dollars % 10;
+		cout << dollars / 5 << " nickels ";
+		dollars = dollars % 5;
+		cout << dollars / 1 << " pennies ";
+	}
+	else if (dollars / 10 != 0)
+	{
+		dollars / 100;
+		dollars = dollars % 100;
+		dollars / 25;
+		dollars = dollars % 25;
+		cout << "your change is " << dollars / 10 << " dimes ";
+		dollars = dollars % 10;
+		cout << dollars / 5 << " nickels ";
+		dollars = dollars % 5;
+		cout << dollars / 1 << " pennies ";
+	}
+	else if (dollars / 5 != 0)
+	{
+		dollars / 100;
+		dollars = dollars % 100;
+		dollars / 25;
+		dollars =dollars % 25;
+		dollars / 10;
+		dollars = dollars % 10;
+		dollars / 5;
+		dollars = dollars % 5;
+		cout << "your change is " << dollars / 1 << " pennies\n";
+	}
+	  
+	
+
 
 
 
